d43a0cd9e526f86ba3f986d3b9e164a3c9a4b1b0
brad <tfssec46@gmail.com>
2023-09-15 09:27:43 -0500

    Brad - commit

--- phishbot.py
+++ phishbot.py
@@ -128,7 +128,7 @@ def create_log(email_from,email_to,subject,url,body):
     print(colors.GREEN + "Create log entry ... \n" + colors.ENDC)
     logdate = (datetime.now()).strftime("%m/%d/%Y %H:%M:%S")
     log_output = logdate + " sender=" + email_from + " recipient=" + email_to + " subject=" + subject + " attachment= phishurl=" + url + " body=" + body + "\n"
-    l = open("/opt/phishbot/log/phishbot.log", "a")
+    l = open("/opt/hackathon/phishbot.log", "a")
     l.write(log_output)
     l.close
 
